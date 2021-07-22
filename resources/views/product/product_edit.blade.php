3eb39e01214eaf3fa5f5aab43dd9971be1d5eee2
dannosm <danno.sm@grob.media>
2021-07-22 17:17:51 +0700

    product

---
+++ resources/views/product/product_edit.blade.php
@@ -0,0 +1,78 @@
+@extends('layout.app')
+@section('content')
+<br>
+<div id="flHorizontalForm" class="col-lg-12 layout-spacing">
+                            <div class="statbox widget box box-shadow">
+                                <div class="widget-header">                                
+                                    <div class="row">
+                                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                            <h4>Product
+                                            <a href="{{url('/product')}}" class="btn btn-dark" style="float:right;margin-top: -1%;color:#fff;">Kembali</a>
+                                            </h4>
+                                        </div>                                                                        
+                                    </div>
+                                </div>
+                                <div class="widget-content widget-content-area">
+                                    <form>
+                                        <div class="form-group row mb-4">
+
+                                            <label for="hEmail" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Name</label>
+                                            <div class="col-xl-10 col-lg-9 col-sm-10">
+                                                <input type="text" class="form-control" id="product_name" placeholder="" name="product_name" value="{{$product->product_name}}">
+
+                                                <input type="hidden" class="form-control" id="product_id" placeholder=""  value="{{$product->product_id}}">
+                                                @csrf
+                                            </div>
+                                        </div>
+                                        <div class="form-group row mb-4">
+                                            <label for="hPassword" class="col-xl-2 col-sm-3 col-sm-2 col-form-label">Description</label>
+                                            <div class="col-xl-10 col-lg-9 col-sm-10">
+                                                <textarea class="form-control" name="description" id="description">{{$product->product_description}}</textarea>
+                                            </div>
+                                        </div>
+                                       
+                                        <div class="form-group row">
+                                            <div class="col-sm-10">
+                                                <a href="javascript:void" class="btn btn-primary" id="saveButton">Save</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        @endsection('content')
+
+    @push('jsfooter')
+    <script type="text/javascript">
+$("#saveButton").click(function(){
+   
+    formData = {
+        'product_id':$("#product_id").val(),
+        'product_name':$("#product_name").val(),
+        'description':$("#description").val(),
+        '_token':$("input[name='_token']").val()
+    }
+
+  
+     $.ajax({
+                url:"{{url('/product/update')}}",
+                method: 'POST',
+                data: formData,
+                cache: false,
+                success: function(response) {
+                   response = JSON.parse(response);
+                   if(response.success == true){
+                        alert('Simpan Data Berhasil');
+                        location.reload();
+                   }else{
+                    alert("Gagal Menyimpan Data");
+                   }
+                },error: function (error) {
+                     alert("Terjadi Kesalahan");          
+                }
+           });
+});
+    </script>
+    @endpush
+            
+                                   
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home','HomeController@index');
+Route::get('/product/add','ProductController@index');
+Route::post('/product/save','ProductController@product_save');
+Route::get('/product','ProductController@product_show');
+Route::get('/product/edit/{id}','ProductController@product_edit');
+Route::get('/product/delete/{id}','ProductController@product_delete');
+Route::post('/product/update','ProductController@product_update');
+Route::post('/product/datatable','ProductController@product_datatable');
+Route::get('/product/get/{id}','ProductController@product_get');
+
+
+
+
+
+
